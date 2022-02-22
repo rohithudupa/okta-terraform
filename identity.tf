335621bb4d23a4867ef150ccbe53664bff300cf7
Rohith Udupa R <rohithudupar@gmail.com>
2022-02-22 12:42:24 +0530

    new files
    change

---
+++ identity.tf
@@ -0,0 +1,54 @@
+variable "org_name" {}
+variable "api_token" {}
+variable "base_url" {}
+
+# Enable and configure the Okta provider
+terraform {
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 3.15"
+    }
+  }
+}
+
+provider "okta" {
+    org_name = var.org_name
+    base_url = var.base_url
+    api_token = var.api_token
+}
+
+resource "okta_app_saml" "test" {
+  label                    = "testAcc_replace_with_uuid"
+  sso_url                  = "http://google.com"
+  recipient                = "http://here.com"
+  destination              = "http://its-about-the-journey.com"
+  audience                 = "http://audience.com"
+  subject_name_id_template = "$${user.userName}"
+  subject_name_id_format   = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+  response_signed          = true
+  signature_algorithm      = "RSA_SHA256"
+  digest_algorithm         = "SHA256"
+  honor_force_authn        = false
+  authn_context_class_ref  = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+}
+
+
+resource "okta_idp_saml_key" "test" {
+  x5c = [okta_app_saml.test.certificate]
+}
+
+
+resource "okta_idp_saml" "test" {
+  name                     = "testAcc_replace_with_uuid"
+  acs_type                 = "INSTANCE"
+  sso_url                  = "https://idp.example.com/test"
+  sso_destination          = "https://idp.example.com/test"
+  sso_binding              = "HTTP-POST"
+  username_template        = "idpuser.email"
+  kid                      = okta_idp_saml_key.test.id
+  issuer                   = "https://idp.example.com/issuer"
+  response_signature_scope = "RESPONSE"
+  request_signature_scope  = "REQUEST"
+  max_clock_skew           = 60
+}
